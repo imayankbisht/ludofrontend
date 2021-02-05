098127b31047674babcab2bba4fa1743a2148733
Mayank Bisht <mayankspartan42@gmail.com>
2021-02-05 13:23:16 +0530

    final

--- .history/src/axios_20201105163232.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: "http://18.188.41.190:8000"
-})
-
-export default instance;
--- .history/src/axios_20210113143424.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: "https://ylrwt.sse.codesandbox.io"
-})
-
-export default instance;
